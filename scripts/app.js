8d1e4bdb368d3c619c5e4a08ed0c16ca58d366ab
IvanNakov10 <Vani311220066@gmail.com>
2021-10-19 13:00:05 +0300

    p2plane

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,11 @@
-const anonimus = document.querySelector('#btn1');
 
-anonimus.addEventListener("click", function() {
-  location.href = 'chosingGame.html';
-});
-
-
+function myF(){
+  let x = document.querySelector('#qu');
+  if (x == "Фъстъци") {
+    location.href = "Drive.html";
+  } else if (x == "фъстъци") {
+    location.href = "Drive.html";
+  } else {
+    alert("Try again");
+  }
+}
